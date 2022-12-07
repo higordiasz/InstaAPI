da631b4a4dbba3c1ef5b8dd31eb379c9559fbeb0
Higor Dias Zuqueto <higordiaszuqueto@gmail.com>
2022-12-07 14:27:09 -0300

    CheckEmail/UsernameSuggestion
    Implementation of new features.

--- Create/Check.cs
+++ Create/Check.cs
@@ -117,11 +117,81 @@ namespace InstaAPI.Create.Check
             IReturn ret = new(0, "Check Email method");
             try
             {
-
+                bool preload = await insta.Preload();
+                if (preload)
+                {
+                    string content = insta.PreloadContent();
+                    if (WebHelperClass.CanReadJson(content))
+                    {
+                        insta._sharedData = JsonConvert.DeserializeObject(WebHelperClass.GetJson(content));
+                        string ajax = !String.IsNullOrEmpty(insta._sharedData.rollout_hash.ToString()) ? insta._sharedData.rollout_hash.ToString() : "1";
+                        insta.SetAjax(ajax);
+                    }
+                    else
+                    {
+                        insta.SetAjax(WebHelperClass.GetAjax(content));
+                    }
+                    Dictionary<string, string> sender = new Dictionary<string, string>()
+                    {
+                        { "email", email },
+                        { "username", "" },
+                        { "first_name", "" },
+                        { "opt_into_one_tap", "false" }
+                    };
+                    insta.HeaderCheck();
+                    HttpRequestMessage req = new(HttpMethod.Post, "https://www.instagram.com/api/v1/web/accounts/web_create_ajax/attempt/") { Content = new FormUrlEncodedContent(sender) };
+                    HttpResponseMessage res = await insta.Client.SendAsync(req);
+                    string resContent = await res.Content.ReadAsStringAsync();
+                    if (res.IsSuccessStatusCode)
+                    {
+                        if (resContent.Contains("email_is_taken"))
+                        {
+                            ret.Response = "Email is taken";
+                            ret.Status = -3;
+                        }
+                        else
+                        {
+                            ret.Response = "Email is available";
+                            ret.Status = 1;
+                        }
+                    }
+                    else
+                    {
+                        if (attemp >= 3)
+                        {
+                            if (resContent.IndexOf("Go back to Instagram") > -1)
+                            {
+                                ret.Status = -4;
+                                ret.Response = "Unable to check";
+                                return ret;
+                            }
+                            else
+                            {
+                                ret.Status = -4;
+                                ret.Response = "Unable to check";
+                                return ret;
+                            }
+                        }
+                        else
+                        {
+                            await Task.Delay(TimeSpan.FromSeconds(15));
+                            var i = attemp + 1;
+                            return await insta.CheckEmail(email, i);
+                        }
+                    }
+                }
+                else
+                {
+                    ret.Response = "Unable to load instagram";
+                    ret.Status = -2;
+                }
             }
             catch (Exception err)
             {
-
+                ret.Err = err;
+                insta.ErrWrite(err, "CheckEmail");
+                ret.Status = -1;
+                ret.Response = "An unexpected error has occurred";
             }
             return ret;
         }
@@ -131,11 +201,74 @@ namespace InstaAPI.Create.Check
             IReturn ret = new(0, "Username Suggestion method");
             try
             {
-
+                bool preload = await insta.Preload();
+                if (preload)
+                {
+                    string content = insta.PreloadContent();
+                    if (WebHelperClass.CanReadJson(content))
+                    {
+                        insta._sharedData = JsonConvert.DeserializeObject(WebHelperClass.GetJson(content));
+                        string ajax = !String.IsNullOrEmpty(insta._sharedData.rollout_hash.ToString()) ? insta._sharedData.rollout_hash.ToString() : "1";
+                        insta.SetAjax(ajax);
+                    }
+                    else
+                    {
+                        insta.SetAjax(WebHelperClass.GetAjax(content));
+                    }
+                    Dictionary<string, string> sender = new Dictionary<string, string>()
+                    {
+                        { "email", "" },
+                        { "username", "" },
+                        { "first_name", name },
+                        { "opt_into_one_tap", "false" }
+                    };
+                    insta.HeaderCheck();
+                    HttpRequestMessage req = new(HttpMethod.Post, "https://www.instagram.com/api/v1/web/accounts/web_create_ajax/attempt/") { Content = new FormUrlEncodedContent(sender) };
+                    HttpResponseMessage res = await insta.Client.SendAsync(req);
+                    string resContent = await res.Content.ReadAsStringAsync();
+                    if (res.IsSuccessStatusCode)
+                    {
+                        dynamic json = JsonConvert.DeserializeObject(resContent);
+                        ret.Status = 1;
+                        ret.Response = json.username_suggestions[0];
+                    }
+                    else
+                    {
+                        if (attemp >= 3)
+                        {
+                            if (resContent.IndexOf("Go back to Instagram") > -1)
+                            {
+                                ret.Status = -4;
+                                ret.Response = "Unable to get username";
+                                return ret;
+                            }
+                            else
+                            {
+                                ret.Status = -4;
+                                ret.Response = "Unable to get username";
+                                return ret;
+                            }
+                        }
+                        else
+                        {
+                            await Task.Delay(TimeSpan.FromSeconds(15));
+                            var i = attemp + 1;
+                            return await insta.UsernameSuggestion(name, i);
+                        }
+                    }
+                }
+                else
+                {
+                    ret.Response = "Unable to load instagram";
+                    ret.Status = -2;
+                }
             }
             catch (Exception err)
             {
-
+                ret.Err = err;
+                insta.ErrWrite(err, "UsernameSuggestion");
+                ret.Status = -1;
+                ret.Response = "An unexpected error has occurred";
             }
             return ret;
         }
