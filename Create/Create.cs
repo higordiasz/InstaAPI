f3ba7efa88ccea753eb193b85d1d6143f688e6b0
Higor Dias Zuqueto <higordiaszuqueto@gmail.com>
2022-12-14 15:41:37 -0300

    CreateAccount
    Initial function to Create Account

--- Create/Create.cs
+++ Create/Create.cs
@@ -3,6 +3,7 @@ using InstaAPI.Helpers.Preloads;
 using InstaAPI.Helpers.WebHelper;
 using InstaAPI.Model.Return;
 using InstaAPI.Model.Create;
+using InstaAPI.Create.Check;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using InstaAPI.Helpers.Errors;
 
 namespace InstaAPI.Create
 {
@@ -21,11 +23,11 @@ namespace InstaAPI.Create
         /// <param name="insta">InstaAPI instace</param>
         /// <param name="account">Account to Create</param>
         /// <returns>
-        /// 1 =
-        /// -1 =
-        /// -2 =
-        /// -3 =
-        /// -4 =
+        ///  1 = Username available
+        /// -1 = Unexpected error
+        /// -2 = Unable to load instagram
+        /// -3 = Email is Taken
+        /// -4 = Username is Taken
         /// -5 =
         /// -6 =
         /// -7 =
@@ -37,11 +39,33 @@ namespace InstaAPI.Create
             IReturn ret = new(0, "Create method");
             try
             {
+                var checkUsername = await insta.CheckUsername(account.Username);
+                if (checkUsername.Status == 1)
+                {
+                    var checkEmail = await insta.CheckEmail(account.Email);
+                    if (checkEmail.Status == 1)
+                    {
 
+                    } else
+                    {
+                        ret.Status = -3;
+                        ret.Response = "Email is taken";
+                        return ret;
+                    }
+                }
+                else
+                {
+                    ret.Status = -4;
+                    ret.Response = "Username is taken";
+                    return ret;
+                }
             }
             catch (Exception err)
             {
-
+                ret.Err = err;
+                insta.ErrWrite(err, "CreateAccount");
+                ret.Status = -1;
+                ret.Response = "An unexpected error has occurred";
             }
             return ret;
         }
